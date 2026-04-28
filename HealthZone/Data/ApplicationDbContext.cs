f5c12931f8b951d4863c148a731cc6edce64691f
Amina <abukvic1@etf.unsa.ba>
2026-04-28 18:58:48 +0200

    Dodaje skeleton projekta

---
+++ HealthZone/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthZone.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
